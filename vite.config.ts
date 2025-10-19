74bc30d94e1087f7a467e4ae0b3337285622ade8
aspacerire-debug <aspacerire@gmail.com>
2025-10-19 17:39:40 -0500

    chore(vite): set root to "origen" and outDir to ../dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  // 👇 Le decimos a Vite que la app vive en /origen
+  root: 'origen',
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'origen'),
+    },
+  },
+  build: {
+    // Salida fuera de /origen para que vercel la encuentre bien
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
 })
 
